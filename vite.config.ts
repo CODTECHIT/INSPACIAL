0226881d75cd9be551d2504ce65c96f0232491a8
CODTECHIT <Codtechitsolutions@gmail.com>
2026-04-03 15:52:57 +0530

    Switch to default esbuild minifier in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -32,12 +32,5 @@ export default defineConfig((): UserConfig => ({
     },
     chunkSizeWarningLimit: 1000,
     cssCodeSplit: true,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
   },
 }));
